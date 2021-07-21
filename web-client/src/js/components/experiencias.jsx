4ae6fba8da4acdcc333bbceecafae07dc9b8aaf8
MaGa6969 <jymbo.02@gmail.com>
2021-07-21 15:23:29 +0200

    añadido css en navbar y logo (color fondo y letras)

--- web-client/src/js/components/experiencias.jsx
+++ web-client/src/js/components/experiencias.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export const Experiencias = () => (
-    <div>
+    <div className={"experiencias"}>
         coming soon experiencias
     </div>
 );
--- web-client/src/js/components/navBar.jsx
+++ web-client/src/js/components/navBar.jsx
@@ -3,11 +3,12 @@ import {Link} from "react-router-dom";
 import logo from "./assets/logo.png";
 
 export const NavBar = () => (
-    <nav>
+    <nav className={"navBar"}>
+        <img className={"logo"} src={logo}/>
         <Link to={"/inicio"}>Inicio</Link>
         <Link to={"/about"}>Quiénes somos</Link>
         <Link to={"/experiencias"}>Experiencias</Link>
         <Link to={"/reservas"}>Reservas</Link>
-        <img className={"logo"} src={logo} />
+
     </nav>
 )
