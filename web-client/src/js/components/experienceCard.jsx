563fc1e4d467c24b740f8bf79b57d058235b39a2
Olga Caparros <caparrosolga@gmail.com>
2021-07-22 15:23:09 +0200

    añadiendo props a campos

--- web-client/src/js/components/experienceCard.jsx
+++ web-client/src/js/components/experienceCard.jsx
@@ -11,3 +11,17 @@ export const ExperienceCard = (props) =>
         <div>{props.experiencia.etiqueta}</div>
 
     </div>
+function Card(props){
+        return(
+            <div className="card">
+            <div className="card__body">
+                <img src={props.img} />
+                <h2 className="card__name">{props.name}</h2>
+                <h2 className="card__descripcion">{props.descripcion}</h2>
+                <p  className="card__duracion">{props.precio}</p>
+                <p  className="card__precio">{props.duracion}</p>
+                <h2 className="card__accesibilidad">{props.accesibilidad}</h2>
+                <p  className="card__etiqueta">{props.etiqueta}</p>
+        </div>
+        )
+})
