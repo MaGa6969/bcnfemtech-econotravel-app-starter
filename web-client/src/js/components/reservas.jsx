a1d4513b69080048ef7362143bce6c43424766f6
IngaD89 <inga.demetrashvili.1@iliauni.edu.ge>
2021-07-26 13:35:09 +0200

    inputs de formulario hechos, aun no tienen funcion

--- web-client/src/js/components/reservas.jsx
+++ web-client/src/js/components/reservas.jsx
@@ -1,5 +1,42 @@
 import * as React from "react";
 
-export const Reservas = () => (
-    <div>Aqui va el formulario</div>
-);
+export const Reservas = () => {
+
+    
+
+    return (
+        <form>
+            <label>Actividades</label>
+            <br/>
+            <select>
+                <option value="montanya">Montaña</option>
+                <option value="playa">Playa</option>
+                <option value="ciudad">Ciudad</option>
+            </select>
+            <br/>
+            <label>Fecha</label>
+            <br/>
+            <input type="text"/>
+            <br/>
+            <label>Nombre y Apellidos</label>
+            <br/>
+            <input type="text"/>
+            <br/>
+            <label>Personas</label>
+            <br/>
+            <input type="text"/>
+            <br/>
+            <label>Teléfono de contacto</label>
+            <br/>
+            <input type="text"/>
+            <br/>
+            <label>Email</label>
+            <br/>
+            <input type="text"/>
+            <br/>
+            <button type="submit">Reserva</button>
+
+        </form>
+    )
+
+}
