430b30b60a425567f58ce88a262758686d129279
IngaD89 <inga.demetrashvili.1@iliauni.edu.ge>
2021-07-26 14:31:34 +0200

    funcion onChange añadido al formulario

--- web-client/src/js/components/reservas.jsx
+++ web-client/src/js/components/reservas.jsx
@@ -1,14 +1,30 @@
 import * as React from "react";
+import {useState} from "react";
 
 export const Reservas = () => {
 
-    
+    const [datos, setDatos] = useState({
+        actividades: '',
+        fecha: '',
+        nombre: '',
+        personas: '',
+        telefono: '',
+        email: ''
+    })
+
+    const inputChange = (event) => {
+        setDatos({
+            ...datos,
+            [event.target.name]: event.target.value
+        })
+    }
 
     return (
         <form>
             <label>Actividades</label>
             <br/>
-            <select>
+            <select onChange={inputChange}
+                    name="actividades">
                 <option value="montanya">Montaña</option>
                 <option value="playa">Playa</option>
                 <option value="ciudad">Ciudad</option>
@@ -16,23 +32,43 @@ export const Reservas = () => {
             <br/>
             <label>Fecha</label>
             <br/>
-            <input type="text"/>
+            <input
+                type="text"
+                name="fecha"
+                onChange={inputChange}
+            />
             <br/>
             <label>Nombre y Apellidos</label>
             <br/>
-            <input type="text"/>
+            <input
+                type="text"
+                name="nombre"
+                onChange={inputChange}
+            />
             <br/>
             <label>Personas</label>
             <br/>
-            <input type="text"/>
+            <input
+                type="text"
+                name="personas"
+                onChange={inputChange}
+            />
             <br/>
             <label>Teléfono de contacto</label>
             <br/>
-            <input type="text"/>
+            <input
+                type="text"
+                name="telefono"
+                onChange={inputChange}
+            />
             <br/>
             <label>Email</label>
             <br/>
-            <input type="text"/>
+            <input
+                type="text"
+                name="email"
+                onChange={inputChange}
+            />
             <br/>
             <button type="submit">Reserva</button>
 
