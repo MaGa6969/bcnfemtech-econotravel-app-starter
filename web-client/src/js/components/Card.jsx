35a38318c94b68c0d1b37089fe9c714373d78ea6
MaGa6969 <jymbo.02@gmail.com>
2021-07-26 11:03:02 +0200

    añadiendo la card, funciona

--- web-client/src/js/components/Card.jsx
+++ web-client/src/js/components/Card.jsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 
 
-export function Card () {
+export function Card ({imgSrc, title, descripcion, precio}) {
     return (
         <div className="card">
-            <img src={paseobici} alt=""/>
+            <img src={imgSrc} alt=""/>
             <div className="card__body">
-                <h2 className="card__title">My title</h2>
-                <h2 className="card__descripcion">texto de prueba</h2>
-                <p  className="card__precio">145€</p>
+                <h2 className="card__title">{title}</h2>
+                <p className="card__descripcion">{descripcion}</p>
+                <h3 className="card__precio">{precio}</h3>
             </div>
         </div>
     )
